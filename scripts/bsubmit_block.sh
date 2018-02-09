90e888d001137e64eee5285c98b7cd6d130abdcd
Jan Clemens <clemensjan@googlemail.com>
2018-02-09 16:04:16 +0100

    absolute paths

--- scripts/bsubmit_block.sh
+++ scripts/bsubmit_block.sh
@@ -1,8 +1,10 @@
+
+SCRIPTDIR="/usr/users/clemens10/analysis/scripts"
 DATADIR="/scratch/clemens10/playback/"
 LOGDIR="/scratch/clemens10/playback/log"
 snakemake --timestamp --rerun-incomplete --keep-going --nolock --notemp \
     --jobs 999 \
     --directory . \
-    --jobscript "jobscript.sh" \
-    --cluster "python bsubmit.py {dependencies} $DATADIR $LOGDIR" \
+    --jobscript "$SCRIPTDIR/jobscript.sh" \
+    --cluster "python $SCRIPTDIR/bsubmit.py {dependencies} $DATADIR $LOGDIR" \
     "$@"
