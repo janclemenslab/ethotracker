3655d83a380dcb2a1c4b927a15ddb9b1aed801bd
Jan Clemens <clemensjan@googlemail.com>
2018-03-11 09:20:47 +0100

    minor changes

--- tracker/FlyPursuitChaining.py
+++ tracker/FlyPursuitChaining.py
@@ -18,19 +18,6 @@ import tracker.Tracker as tk
 def init(vr, start_frame, threshold, nflies, file_name, num_bg_frames=1000):
     res = Results()                     # init results object
 
-    # bg_loaded = False
-    # if instance(num_bg_frames, str):
-    #     try:
-    #         background = cv2.imread(num_bg_frames)
-    #         background = bg.background[:, :, 0]
-    #         bg_loaded = True
-    #     except Exception as e:
-    #         print('could not load {file}'.format(num_bg_frames))
-    #         print(e)
-    #
-    # if not bg_loaded:
-    #     # printf('estimating background')
-
     bg = BackGround(vr)
     bg.estimate(num_bg_frames, start_frame)
     res.background = bg.background[:, :, 0]
@@ -107,26 +94,30 @@ class Prc():
             res.frame_count = int(res.frame_count+1)
             foreground = fg.threshold(res.background - frame[:, :, 0], res.threshold * 255)
             foreground = fg.erode(foreground.astype(np.uint8), kernel_size=4)
-
+            # foreground = fg.dilate(foreground.astype(np.uint8), kernel_size=4)
+            # foreground = fg.close(foreground.astype(np.uint8), kernel_size=4)
+            # import ipdb; ipdb.set_trace()
+            # foreground = cv2.blur(foreground.astype(np.uint8)*255.0,(11,11))
             for ii in uni_chambers:
                 if ii > 0:  # 0 is background
                     foreground_cropped = foreground[chamber_slices[ii]] * (res.chambers[chamber_slices[ii]] == ii)  # crop frame to current chamber
                     # centers[ii - 1, :], labels, points, _, area[0, ii - 1] = fg.segment_center_of_mass(foreground_cropped)
-                    centers[ii-1, :, :], labels, points,  = fg.segment_cluster(foreground_cropped, num_clusters= res.nflies)
+                    centers[ii-1, :, :], labels, points,  = fg.segment_cluster(foreground_cropped, num_clusters=res.nflies)
+
                     if points.shape[0] > 0:   # check that there we have not lost the fly in the current frame
                         for label in np.unique(labels):
                             lines[ii-1, label, :, :], _ = tk.fit_line(points[labels[:, 0] == label, :]) # need to make this more robust - based on median center and some pixels around that...
 
             if res.nflies > 1 and old_centers is not None and old_lines is not None:  # match centers across frames - not needed for one fly per chamber
                 for ii in uni_chambers:
-                    if ii>0:
+                    if ii > 0:
                         new_labels, centers[ii-1, :, :] = tk.match(old_centers[ii-1, :, :], centers[ii-1, :, :])
-                        lines[ii-1, :, :, :] = lines[ii-1, new_labels, :, :] # also re-order lines
+                        lines[ii-1, :, :, :] = lines[ii-1, new_labels, :, :]  # also re-order lines
             old_centers = np.copy(centers)  # remember
 
             if old_lines is not None:  # fix forward/backward flips
                 for ii in uni_chambers:
-                    if ii>0:
+                    if ii > 0:
                         if points.shape[0] > 0:   # check that we have not lost all flies in the current frame
                             for label in np.unique(labels):
                                 lines[ii-1, label, :, :], is_flipped, D = tk.fix_flips(old_lines[ii-1, label, 0, :], lines[ii-1, label, :, :])
@@ -138,7 +129,7 @@ class Prc():
             yield res, foreground
 
 
-def run(file_name, override=False, init_only=False, display=None, save_video=False, nflies=1, threshold=0.4, save_interval=1000, start_frame=50, led_coords=[10, 550, 100, -1]):
+def run(file_name, override=False, init_only=False, display=None, save_video=False, nflies=1, threshold=0.4, save_interval=1000, start_frame=0, led_coords=[10, 550, 100, -1]):
     try:
         printf = lambda string: print(os.path.basename(file_name) + ": " + string)
         printf('processing ' + file_name)
@@ -165,7 +156,6 @@ def run(file_name, override=False, init_only=False, display=None, save_video=Fal
             led_coords = fg.detect_led(frame)
             vr.reset()
         print(led_coords)
-
         if init_only:
             return
         res.threshold = threshold
@@ -181,7 +171,7 @@ def run(file_name, override=False, init_only=False, display=None, save_video=Fal
         start = time.time()
         ret = True
         frame_processor = Prc(res)
-        vr.reset()
+
         while(ret and res.frame_count < vr.number_of_frames + 1001):
             ret, frame = vr.read()
             if not ret:
@@ -201,12 +191,13 @@ def run(file_name, override=False, init_only=False, display=None, save_video=Fal
                         frame_with_tracks = fg.annotate(frame[40:,:,:]/255,
                                                        centers=np.clip(np.uint(res.centers[res.frame_count, chamberID, :, :]),0,10000),
                                                        lines=np.clip(np.uint(res.lines[res.frame_count, chamberID, 0:res.lines.shape[2], :, :]),0,10000))
-                        # frame_with_tracks = fg.annotate(cv2.cvtColor(np.uint8(foreground), cv2.COLOR_GRAY2RGB),
+                        # frame_with_tracks = fg.annotate(cv2.cvtColor(np.uint8(foreground[40:,:]), cv2.COLOR_GRAY2RGB).astype(np.float32),
                         #                                 centers=np.clip(np.uint(res.centers[res.frame_count, chamberID, :, :]),0,10000),
                         #                                 lines=np.clip(np.uint(res.lines[res.frame_count, chamberID, 0:res.lines.shape[2], :, :]),0,10000))
 
                     # display annotated frame
                     if display is not None and res.frame_count % display == 0:
+                        # fg.show(np.float32(frame_with_tracks)/255.0, autoscale=False)
                         fg.show(frame_with_tracks)
 
                     # save annotated frame to video
