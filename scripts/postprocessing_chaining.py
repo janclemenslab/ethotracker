7b43e8f157ca6b6e39aaaad0f0a988aa6b0670af
Jan Clemens <clemensjan@googlemail.com>
2018-03-14 19:17:34 +0100

    fix orientations and calculate chaining index

--- scripts/postprocessing_chaining.py
+++ scripts/postprocessing_chaining.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import numpy as np
 import h5py
-import scipy.signal
+import scipy
 import sys
 import peakutils
 from scipy import signal
@@ -12,11 +12,13 @@ def load_data(file_name):
     with h5py.File(file_name, 'r') as f:
         pos = f['centers'][:]
         led = f['led'][:]
+        lines = f['lines'][:]
         start_frame = f['start_frame'].value
     pos = pos[start_frame + 1:-1000, :, :]
     led = led[start_frame + 1:-1000, 0].T
+
     nflies = pos.shape[1]
-    return pos, led, nflies
+    return pos, lines, led, nflies
 
 
 def parse_prot(filename):
@@ -92,6 +94,110 @@ def chunk_data(data, cutpoints, chunklen):
     return data_chunks
 
 
+def smooth(x, N):
+    return np.convolve(x, np.ones((N,))/N, mode='full')[(N-1):]
+
+
+def fix_orientations(lines0, chamber_number=0):
+    # fix orientation direction of flies
+    nflies = lines0.shape[3]
+    vel = np.zeros((lines0.shape[0], nflies))
+    ori = np.zeros((lines0.shape[0], 2, nflies))
+    lines_fixed = lines0.copy()
+
+    for fly in range(nflies):
+        # get fly lines and smooth
+        lines = lines0[:,chamber_number, fly,:,:].astype(np.float64)  # time x [head,tail] x [x,y]
+        for h in range(2):
+            for p in range(2):
+                lines[:,h,p] = smooth(lines[:,h,p], 10)
+
+        # get fly movement and smooth
+        dpos = np.gradient(lines[:,0,:], axis=0)  # change of head position over time - `np.gradient` is less noisy than `np.diff`
+        for p in range(2):
+            dpos[:,p] = smooth(dpos[:,p], 10)
+
+        # get fly orientation
+        ori[:,:,fly] = np.diff(lines, axis=1)[:,0,:]  # orientation of fly: head pos - tail pos, `[:,0,:]` cuts off trailing dim
+        ori_norm = np.linalg.norm(ori[:,:,fly], axis=1)  # "length" of the fly
+
+        # dpos_norm = np.linalg.norm(dpos, axis=1)
+        # alignment (dot product) between movement and orientation
+        dpos_ori = np.einsum('ij,ij->i', ori[:,:,fly], dpos)  #"element-wise" dot product between orientation and velocity vectors
+        vel[:, fly] = dpos_ori/ori_norm  # normalize by fly length (norm of ori (head->tail) vector)
+
+        # 1. clean up velocity - only consider epochs were movement is fast and over a prolonged time
+        orichange = np.diff(np.unwrap(np.arctan2(ori[:,0,fly], ori[:,1,fly])))  # change in orientation for change point detection
+        velsmooththres = smooth(vel[:,fly],20)  # smooth velocity
+        velsmooththres[np.abs(velsmooththres)<0.4] = 0  # threshold - keep only "fast" events to be more robust
+        velsmooththres = scipy.signal.medfilt(velsmooththres, 5)  # median filter to get rid of weird, fast spikes in vel
+
+        # 2. detect the range of points during which velocity changes in sign
+        idx, = np.where(velsmooththres!=0)  # indices where vel is high
+        switchpoint = np.gradient(np.sign(velsmooththres[idx]))  # changes in the sign of the thres vel
+        switchtimes = idx[np.where(switchpoint!=0)]  # indices where changes on vel sign occurr
+        switchpoint = switchpoint[switchpoint!=0]    # sign of the change in vel
+
+        # 3. detect actual change point with that range
+        changepoints = []  # fill this with
+        for cnt in range(0, len(switchtimes[:-1]),2):
+            # define change points as maxima in orientation changes between switchs in direction of motion
+            changepoints.append(switchtimes[cnt]+np.argmax(np.abs(orichange[switchtimes[cnt]-1:switchtimes[cnt+1]])))
+            # mark change points for interpolation
+            velsmooththres[changepoints[-1]-1] = -switchpoint[cnt]
+            velsmooththres[changepoints[-1]] = switchpoint[cnt+1]
+
+        # 4. fill values using change points - `-1` means we need to swap head and tail
+        idx, = np.where(velsmooththres!=0)  # update `idx` to include newly marked change points
+        f = scipy.interpolate.interp1d(idx, velsmooththres[idx], kind="nearest", fill_value="extrapolate");
+        idx_new, = np.where(velsmooththres==0)
+        ynew = f(range(velsmooththres.shape[0]))
+
+        # 4. swap head and tail
+        lines_fixed[ynew<0,chamber_number,fly,:,:] = lines_fixed[ynew<0,chamber_number,fly,::-1,:]
+    return lines_fixed
+
+
+def get_chaining(lines, chamber_number=0):
+    nframes = lines.shape[0]
+    nflies = lines.shape[2]
+    tails = lines[:,chamber_number,:,0,:]
+    heads = lines[:,chamber_number,:,1,:]
+
+    D_h2t = np.zeros((nflies, nflies, nframes))
+    D_h2h = np.zeros((nflies, nflies, nframes))
+    Dc = np.zeros((nflies, nflies, nframes), dtype=np.bool)
+    Dh = np.zeros((nflies, nflies, nframes), dtype=np.bool)
+
+    chainee = -np.ones((nflies*nflies, nframes), dtype=np.int16)
+    chainer = -np.ones((nflies*nflies, nframes), dtype=np.int16)
+    headee = -np.ones((nflies*nflies, nframes), dtype=np.int16)
+    header = -np.ones((nflies*nflies, nframes), dtype=np.int16)
+
+    for frame_number in range(0,nframes):
+        T = frame_number
+        D_h2t[:,:,T] = scipy.spatial.distance.cdist(tails[T,:,:], heads[T,:,:], metric='euclidean')
+        D_h2h[:,:,T] = scipy.spatial.distance.cdist(heads[T,:,:], heads[T,:,:], metric='euclidean')
+
+        flylength = np.diag(D_h2t[:,:,T])  # diagonals contain tail->head distances=fly lengths
+        min_distance = np.min(flylength)  # interaction distance is flylength+x
+        Dc[:,:,T] = D_h2t[:,:,T]<min_distance   # chaining
+        Dh[:,:,T] = D_h2h[:,:,T]<min_distance/2 # head-butting
+        # ignore diagonal entries
+        np.fill_diagonal(Dc[:,:,T], False)
+        np.fill_diagonal(Dh[:,:,T], False)
+
+        # get x,y coords of interacting flies
+        chainee_this, chainer_this = np.where(Dc[:,:,T])
+        headee_this, header_this = np.where(Dh[:,:,T])
+
+        # save all in list
+        chainee[0:chainee_this.shape[0],T] = chainee_this
+        chainer[0:chainer_this.shape[0],T] = chainer_this
+        headee[0:headee_this.shape[0],T] = headee_this
+        header[0:header_this.shape[0],T] = header_this
+    return chainee, chainer, headee, header, D_h2t, D_h2h, Dc, Dh
+
 def stats_by_group(data, grouping, fun):
     # TODO: pass through args and kwargs for fun
 
@@ -116,7 +222,23 @@ if __name__ == '__main__':
     print('processing tracks in {0} with playlist {1}. will save to {2}'.format(track_file_name, prot_file_name, save_file_name))
 
     # read tracking data
-    pos, led, nflies = load_data(track_file_name)
+    pos, lines, led, nflies = load_data(track_file_name)
+    # fix lines and get chaining IndexError
+    lines_fixed = fix_orientations(lines)
+    chainee, chainer, headee, header, D_h2t, D_h2h, Dc, Dh = get_chaining(lines_fixed, chamber_number=0)
+    # save fixed lines and chaining data_chunks
+    print(f'saving chaining data to {save_file_name}')
+    with h5py.File(save_file_name, 'w') as f:
+        f.create_dataset('lines_fixed', data=lines_fixed, compression='gzip')
+        f.create_dataset('chainer', data=chainer, compression='gzip')
+        f.create_dataset('chainee', data=chainee, compression='gzip')
+        f.create_dataset('header', data=header, compression='gzip')
+        f.create_dataset('headee', data=headee, compression='gzip')
+        f.create_dataset('D_h2t', data=D_h2t, compression='gzip')
+        f.create_dataset('D_h2h', data=D_h2h, compression='gzip')
+        f.create_dataset('Dc', data=Dc, compression='gzip')
+        f.create_dataset('Dh', data=Dh, compression='gzip')
+
     # detect LED onsets
     led_onsets, led_offsets = get_led_peaks(led[0], thres=0.8, min_interval=1000)
     print(led_onsets)
