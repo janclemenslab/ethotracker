4d06b3be1501719ba6d16b1e38473d34dc7dde0c
Jan Clemens <clemensjan@googlemail.com>
2018-03-19 18:12:03 +0100

    refactored code

---
+++ post/chainingindex.py
@@ -0,0 +1,85 @@
+import numpy as np
+import scipy.spatial
+
+
+def get_chaining(lines, chamber_number=0):
+    nframes = lines.shape[0]
+    nflies = lines.shape[2]
+    tails = lines[:,chamber_number,:,0,:]
+    heads = lines[:,chamber_number,:,1,:]
+
+    D_h2t = np.zeros((nflies, nflies, nframes))
+    D_h2h = np.zeros((nflies, nflies, nframes))
+    Dc = np.zeros((nflies, nflies, nframes), dtype=np.bool)
+    Dh = np.zeros((nflies, nflies, nframes), dtype=np.bool)
+
+    chainee = -np.ones((nflies*nflies, nframes), dtype=np.int16)
+    chainer = -np.ones((nflies*nflies, nframes), dtype=np.int16)
+    headee = -np.ones((nflies*nflies, nframes), dtype=np.int16)
+    header = -np.ones((nflies*nflies, nframes), dtype=np.int16)
+
+    for frame_number in range(0,nframes):
+        T = frame_number
+        D_h2t[:,:,T] = scipy.spatial.distance.cdist(tails[T,:,:], heads[T,:,:], metric='euclidean')
+        D_h2h[:,:,T] = scipy.spatial.distance.cdist(heads[T,:,:], heads[T,:,:], metric='euclidean')
+
+        flylength = np.diag(D_h2t[:,:,T])  # diagonals contain tail->head distances=fly lengths
+        min_distance = np.min(flylength)  # interaction distance is flylength+x
+        Dc[:,:,T] = D_h2t[:,:,T]<min_distance   # chaining
+        Dh[:,:,T] = D_h2h[:,:,T]<min_distance/2 # head-butting
+        # ignore diagonal entries
+        np.fill_diagonal(Dc[:,:,T], False)
+        np.fill_diagonal(Dh[:,:,T], False)
+
+        # get x,y coords of interacting flies
+        chainee_this, chainer_this = np.where(Dc[:,:,T])
+        headee_this, header_this = np.where(Dh[:,:,T])
+
+        # save all in list
+        chainee[0:chainee_this.shape[0],T] = chainee_this
+        chainer[0:chainer_this.shape[0],T] = chainer_this
+        headee[0:headee_this.shape[0],T] = headee_this
+        header[0:header_this.shape[0],T] = header_this
+    return chainee, chainer, headee, header, D_h2t, D_h2h, Dc, Dh
+
+
+def get_chainlength(chainer, chainee, nflies):
+    # calculate chain length
+    # TODO: [-] this will ignore circles since there is no seed - a fly that chains but is not a chainee
+    #       [x] very slow... - will run endlessly if there is a loop
+    nframes = chainer.shape[1]
+
+    chain_length = np.zeros((nflies*2, nframes), dtype=np.uint16)
+    for frame_number in range(0, nframes):
+        # if frame_number % 10000==0:
+        #     print(frame_number)
+        # find starters of chain - flies that are chainer but not chainees
+        chain_seeds = [x for x in chainer[:, frame_number] if not x in chainee[:, frame_number]]
+
+        # for each starter - follow the chain to the end
+        chain = [-1]*len(chain_seeds)
+        for chain_count, chain_seed in enumerate(chain_seeds):
+            chain[chain_count] = [chain_seed]
+            chain_link = [chain_seed]
+            idx = 1
+            while len(chain_link):
+                idx, = np.where(chainer[:, frame_number] == chain_link)  # find chainee for current chainer
+                chain_link = chainee[idx, frame_number].tolist()  # this is the next chainee
+                # TODO: remove already counted chainers so we don't count them multiple times
+                try:
+                    if chain_link[0] not in chain[chain_count]:
+                        chain[chain_count].append(chain_link[0])
+                    else:
+                        break  # avoid loop where chainee chains chainer
+                except IndexError as e:
+                    pass
+        # find chainers that are not accounted for - these may be part of a circle...
+        flat_list = [item for ch in chain for item in ch[:-1]]  # flatten list excluding last in chain
+        leftovers = [x for x in flat_list if not x in chainer[:, frame_number]]  # count
+        if leftovers:
+            chain.append(leftovers)
+
+        this_chain_len = [len(x) for x in chain]
+        chain_length[:len(this_chain_len), frame_number] = this_chain_len
+    #     print(f"found {len(chain_seeds)} chains with {this_chain_len} flies: {chain}")
+    return chain_length
--- scripts/postprocessing_chaining.py
+++ scripts/postprocessing_chaining.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 import numpy as np
 import h5py
-import scipy
 import sys
 import peakutils
-from scipy import signal
 import os
-import networkx as nx
-import itertools
+from post.networkmotifs import *
+from post.chainingindex import *
+from post.fixtracks import *
+# import networkx as nx
+# import itertools
+
 
 def load_data(file_name):
     with h5py.File(file_name, 'r') as f:
@@ -53,6 +55,7 @@ def get_led_peaks(led, thres=0.8, min_interval=0):
     led_offsets = led_offsets[led_offset_interval > min_interval]
     return led_onsets, led_offsets
 
+
 def plot_led_peaks(led, led_onsets, led_offset, savefilename=None):
     import matplotlib.pyplot as plt
     # fig = plt.gcf()
@@ -77,10 +80,12 @@ def plot_led_peaks(led, led_onsets, led_offset, savefilename=None):
         print(savefilename)
         plt.savefig(savefilename)
 
+
 def get_speed(pos, medfiltlen=None):
     spd = np.sqrt(np.sum(np.diff(pos, axis=0).astype(np.float32)**2, axis=2))
     if medfiltlen is not None:
-        spd = scipy.signal.medfilt(spd, medfiltlen)
+        for fly in range(spd.shape[1]):
+            spd[:, fly] = scipy.signal.medfilt(spd[:, fly], medfiltlen)
     return spd
 
 
@@ -93,252 +98,6 @@ def chunk_data(data, cutpoints, chunklen):
         cnt += this.shape[1]
     data_chunks = data_chunks[:, :cnt]  # keep only non-empty traces
     return data_chunks
-
-
-def smooth(x, N):
-    return np.convolve(x, np.ones((N,))/N, mode='full')[(N-1):]
-
-
-def fix_orientations(lines0, chamber_number=0):
-    # fix orientation direction of flies
-    nflies = lines0.shape[3]
-    vel = np.zeros((lines0.shape[0], nflies))
-    ori = np.zeros((lines0.shape[0], 2, nflies))
-    lines_fixed = lines0.copy()
-
-    for fly in range(nflies):
-        # get fly lines and smooth
-        lines = lines0[:,chamber_number, fly,:,:].astype(np.float64)  # time x [head,tail] x [x,y]
-        for h in range(2):
-            for p in range(2):
-                lines[:,h,p] = smooth(lines[:,h,p], 10)
-
-        # get fly movement and smooth
-        dpos = np.gradient(lines[:,0,:], axis=0)  # change of head position over time - `np.gradient` is less noisy than `np.diff`
-        for p in range(2):
-            dpos[:,p] = smooth(dpos[:,p], 10)
-
-        # get fly orientation
-        ori[:,:,fly] = np.diff(lines, axis=1)[:,0,:]  # orientation of fly: head pos - tail pos, `[:,0,:]` cuts off trailing dim
-        ori_norm = np.linalg.norm(ori[:,:,fly], axis=1)  # "length" of the fly
-
-        # dpos_norm = np.linalg.norm(dpos, axis=1)
-        # alignment (dot product) between movement and orientation
-        dpos_ori = np.einsum('ij,ij->i', ori[:,:,fly], dpos)  #"element-wise" dot product between orientation and velocity vectors
-        vel[:, fly] = dpos_ori/ori_norm  # normalize by fly length (norm of ori (head->tail) vector)
-
-        # 1. clean up velocity - only consider epochs were movement is fast and over a prolonged time
-        orichange = np.diff(np.unwrap(np.arctan2(ori[:,0,fly], ori[:,1,fly])))  # change in orientation for change point detection
-        velsmooththres = smooth(vel[:,fly],20)  # smooth velocity
-        velsmooththres[np.abs(velsmooththres)<0.4] = 0  # threshold - keep only "fast" events to be more robust
-        velsmooththres = scipy.signal.medfilt(velsmooththres, 5)  # median filter to get rid of weird, fast spikes in vel
-
-        # 2. detect the range of points during which velocity changes in sign
-        idx, = np.where(velsmooththres!=0)  # indices where vel is high
-        switchpoint = np.gradient(np.sign(velsmooththres[idx]))  # changes in the sign of the thres vel
-        switchtimes = idx[np.where(switchpoint!=0)]  # indices where changes on vel sign occurr
-        switchpoint = switchpoint[switchpoint!=0]    # sign of the change in vel
-
-        # 3. detect actual change point with that range
-        changepoints = []  # fill this with
-        for cnt in range(0, len(switchtimes[:-1]),2):
-            # define change points as maxima in orientation changes between switchs in direction of motion
-            changepoints.append(switchtimes[cnt]+np.argmax(np.abs(orichange[switchtimes[cnt]-1:switchtimes[cnt+1]])))
-            # mark change points for interpolation
-            velsmooththres[changepoints[-1]-1] = -switchpoint[cnt]
-            velsmooththres[changepoints[-1]] = switchpoint[cnt+1]
-
-        # 4. fill values using change points - `-1` means we need to swap head and tail
-        idx, = np.where(velsmooththres!=0)  # update `idx` to include newly marked change points
-        f = scipy.interpolate.interp1d(idx, velsmooththres[idx], kind="nearest", fill_value="extrapolate");
-        idx_new, = np.where(velsmooththres==0)
-        ynew = f(range(velsmooththres.shape[0]))
-
-        # 4. swap head and tail
-        lines_fixed[ynew<0,chamber_number,fly,:,:] = lines_fixed[ynew<0,chamber_number,fly,::-1,:]
-    return lines_fixed
-
-
-def get_chaining(lines, chamber_number=0):
-    nframes = lines.shape[0]
-    nflies = lines.shape[2]
-    tails = lines[:,chamber_number,:,0,:]
-    heads = lines[:,chamber_number,:,1,:]
-
-    D_h2t = np.zeros((nflies, nflies, nframes))
-    D_h2h = np.zeros((nflies, nflies, nframes))
-    Dc = np.zeros((nflies, nflies, nframes), dtype=np.bool)
-    Dh = np.zeros((nflies, nflies, nframes), dtype=np.bool)
-
-    chainee = -np.ones((nflies*nflies, nframes), dtype=np.int16)
-    chainer = -np.ones((nflies*nflies, nframes), dtype=np.int16)
-    headee = -np.ones((nflies*nflies, nframes), dtype=np.int16)
-    header = -np.ones((nflies*nflies, nframes), dtype=np.int16)
-
-    for frame_number in range(0,nframes):
-        T = frame_number
-        D_h2t[:,:,T] = scipy.spatial.distance.cdist(tails[T,:,:], heads[T,:,:], metric='euclidean')
-        D_h2h[:,:,T] = scipy.spatial.distance.cdist(heads[T,:,:], heads[T,:,:], metric='euclidean')
-
-        flylength = np.diag(D_h2t[:,:,T])  # diagonals contain tail->head distances=fly lengths
-        min_distance = np.min(flylength)  # interaction distance is flylength+x
-        Dc[:,:,T] = D_h2t[:,:,T]<min_distance   # chaining
-        Dh[:,:,T] = D_h2h[:,:,T]<min_distance/2 # head-butting
-        # ignore diagonal entries
-        np.fill_diagonal(Dc[:,:,T], False)
-        np.fill_diagonal(Dh[:,:,T], False)
-
-        # get x,y coords of interacting flies
-        chainee_this, chainer_this = np.where(Dc[:,:,T])
-        headee_this, header_this = np.where(Dh[:,:,T])
-
-        # save all in list
-        chainee[0:chainee_this.shape[0],T] = chainee_this
-        chainer[0:chainer_this.shape[0],T] = chainer_this
-        headee[0:headee_this.shape[0],T] = headee_this
-        header[0:header_this.shape[0],T] = header_this
-    return chainee, chainer, headee, header, D_h2t, D_h2h, Dc, Dh
-
-
-def get_chainlength(chainer, chainee, nflies):
-    # calculate chain length
-    # TODO: [-] this will ignore circles since there is no seed - a fly that chains but is not a chainee
-    #       [x] very slow... - will run endlessly if there is a loop
-    nframes = chainer.shape[1]
-
-    chain_length = np.zeros((nflies*2, nframes), dtype=np.uint16)
-    for frame_number in range(0, nframes):
-        # if frame_number % 10000==0:
-        #     print(frame_number)
-        # find starters of chain - flies that are chainer but not chainees
-        chain_seeds = [x for x in chainer[:, frame_number] if not x in chainee[:, frame_number]]
-
-        # for each starter - get chain
-        chain = [-1]*len(chain_seeds)
-        for chain_count, chain_seed in enumerate(chain_seeds):
-            chain[chain_count] = [chain_seed]
-            chain_link = [chain_seed]
-            idx = 1
-            while len(chain_link):
-                idx, = np.where(chainer[:, frame_number] == chain_link)
-                chain_link = chainee[idx, frame_number].tolist()
-                try:
-                    if chain_link[0] not in chain[chain_count]:
-                        chain[chain_count].append(chain_link[0])
-                    else:
-                        break  # avoid loop where chainee chains chainer
-                except IndexError as e:
-                    pass
-        # find chainers that are not accounted for - these may be part of a circle...
-        flat_list = [item for ch in chain for item in ch[:-1]]  # flatten list excluding last in chain
-        leftovers = [x for x in flat_list if not x in chainer[:, frame_number]]  # count
-        if leftovers:
-            chain.append(leftovers)
-
-        this_chain_len = [len(x) for x in chain]
-        chain_length[:len(this_chain_len), frame_number] = this_chain_len
-    #     print(f"found {len(chain_seeds)} chains with {this_chain_len} flies: {chain}")
-    return chain_length
-
-
-def motif_counter(gr, mo, k):
-    """Counts motifs in a directed graph
-    :param gr: A ``DiGraph`` object
-    :param mo: A ``dict`` of motifs to count
-    :param k: An ``int`` specifying motif length
-    :returns: A ``dict`` with the number of each motifs, with the same keys as ``mo``
-    This function is actually rather simple. It will extract all 3-grams from
-    the original graph, and look for isomorphisms in the motifs contained
-    in a dictionary. The returned object is a ``dict`` with the number of
-    times each motif was found.::
-        >>> print mcounter(gr, mo)
-        {'S1': 4, 'S3': 0, 'S2': 1, 'S5': 0, 'S4': 3}
-    """
-    # from: https://gist.github.com/tpoisot/8582648
-    #This function will take each possible subgraph of gr of size 3, then
-    #compare them to the mo dict using .subgraph() and is_isomorphic
-    #This line simply creates a dictionary with 0 for all values, and the
-    #motif names as keys
-    mcount = dict(zip(mo.keys(), list(map(int, np.zeros(len(mo))))))
-    nodes = gr.nodes()
-
-    #We use iterools.product to have all combinations of three nodes in the
-    #original graph. Then we filter combinations with non-unique nodes, because
-    #the motifs do not account for self-consumption.
-    triplets = list(itertools.product(*[nodes]*k))
-    triplets = [trip for trip in triplets if len(list(set(trip))) == k]
-    triplets = map(list, map(np.sort, triplets))
-    u_triplets = []
-    [u_triplets.append(trip) for trip in triplets if not u_triplets.count(trip)]
-
-    #The for each each of the triplets, we (i) take its subgraph, and compare
-    #it to all the possible motifs
-    for trip in u_triplets:
-        sub_gr = gr.subgraph(trip)
-        mot_match = list(map(lambda mot_id: nx.is_isomorphic(sub_gr, mo[mot_id]), mo.keys()))
-        match_keys = [list(mo.keys())[i] for i in range(len(mo)) if mot_match[i]]
-        if len(match_keys) == 1:
-            mcount[match_keys[0]] += 1
-    return mcount
-
-
-def generate_motifs(k):
-    combs = list(itertools.permutations(range(1,k+1), 2)) # all node combinations = all possible edges
-    motifs = {}
-    cnt = -1
-    for kk in range(1,k+1):  # number of edges in motif
-        combscombs = list(itertools.combinations(combs, kk))  # all node combinations with kk edges
-        for cc in combscombs:  # now filter motifs
-            # make sure the motif contains all nodes
-            a = []
-            [a.extend(list(c)) for c in cc]  # flatten list
-            all_edges_contained = np.unique(a).shape[0]>=k
-            # make sure each edge exists only once in one direction in the motif
-            aa = [sorted(c) for c in list(cc)]  # list of all sorted (undirected) edges
-            all_edges_unique = len(aa)==np.unique(aa, axis=0).shape[0]  # True only if all edges are unique
-            # make sure the motif is a fully connected graph
-            fully_connected = len(aa)>=k-1
-            # check if a topologically similar motif is alread in list
-            mot_match = list(map(lambda mot_id: nx.is_isomorphic(nx.DiGraph(list(cc)), motifs[mot_id]), motifs.keys()))
-            motif_is_new = not(any(mot_match))
-
-            # only add motif if it matches all of the above criteria
-            if all_edges_contained and all_edges_unique and motif_is_new and fully_connected:
-                cnt += 1
-                motifs[f"S{k}-{cnt}"] = nx.DiGraph(list(cc))
-    return motifs
-
-
-def process_motifs(chainer, chainee, max_k=4):
-    # autogenerate network motifs up to size max_k
-    motifs = [0]*(max_k+1)
-    nframes = chainer.shape[1]
-    motif_counts_all = []
-    for k in range(2,max_k+1):
-        motifs[k] = generate_motifs(k)
-        print(f"There exist {len(motifs[k])} topologically unique, directed motifs of size {k}")
-
-    # motifs in all frames
-    nmotifs = sum([len(b) for b in motifs[2:]])
-    motif_counts = np.zeros((nmotifs, nframes), dtype=np.uint16)
-    # initialize - add all flies as nodes
-    for frame_number in range(nframes):
-        if frame_number % 10000 == 0:
-            print(frame_number)
-
-        G = nx.DiGraph()
-        # add chaining connections
-        this_chainer = chainer[chainer[:, frame_number]>=0, frame_number]
-        this_chainee = chainee[chainer[:, frame_number]>=0, frame_number]
-        for tcher, tchee in zip(this_chainer, this_chainee):
-            G.add_edge(tcher, tchee)
-
-        motif_counts_this_all = np.zeros((0,), dtype=np.uint16)
-        for k in range(2, min(len(this_chainer)+1, max_k+1)):
-            this_motif_counts = motif_counter(G, motifs[k], k)
-            motif_counts_this_all = np.concatenate( (motif_counts_this_all, list(this_motif_counts.values())))
-        motif_counts[:len(motif_counts_this_all),frame_number] = motif_counts_this_all
-    return motif_counts, motifs
 
 
 def stats_by_group(data, grouping, fun):
@@ -363,7 +122,7 @@ if __name__ == '__main__':
     prot_file_name = sys.argv[2]
     save_file_name = sys.argv[3]
     print('processing tracks in {0} with playlist {1}. will save to {2}'.format(track_file_name, prot_file_name, save_file_name))
-
+    chamber_number = 0
     # read tracking data
     pos, lines, led, nflies = load_data(track_file_name)
     # fix lines and get chaining IndexError
@@ -388,7 +147,7 @@ if __name__ == '__main__':
     motif_counts, motifs = process_motifs(chainer, chainee, max_k=4)
     with h5py.File(save_file_name, 'a') as f:
         f.create_dataset('motif_counts', data=motif_counts, compression='gzip')
-        f.create_dataset('motifs', data=motifs, compression='gzip')
+        # f.create_dataset('motifs', data=motifs, compression='gzip')
 
     # detect LED onsets
     led_onsets, led_offsets = get_led_peaks(led[0], thres=0.8, min_interval=1000)
@@ -401,7 +160,7 @@ if __name__ == '__main__':
 
     if len(led_onsets):
         print('found {0} led onsets'.format(len(led_onsets)))
-        spd = get_speed(pos[:, 0, :, :], 7)
+        spd = get_speed(pos[:, chamber_number, :, :])
         # chunk data
         chunklen = 13000
         chunkpre = 3000
