088147ad32a6a2d6b343bf24bfcb994d1011a964
Jan Clemens <clemensjan@googlemail.com>
2018-03-27 17:04:32 +0200

    reformat, median bg

--- tracker/BackGround.py
+++ tracker/BackGround.py
@@ -4,8 +4,10 @@ from .VideoReader import video_file
 import argparse
 import os
 
+
 class BackGround():
-    """class for estimating background - used median
+    """Calculate background as mean over frames.
+
        ARGS
           vr - VideoReader instance
        VARS
@@ -19,34 +21,35 @@ class BackGround():
     """
 
     def __init__(self, vr):
-        """constructor - vr is VideoReader instance"""
+        """Constructor - vr is VideoReader instance."""
         self.vr = vr
         self.background = np.zeros((self.vr.frame_width, self.vr.frame_height, self.vr.frame_channels))
         self.background_count = 0
-        # self.frames = []
 
     def estimate(self, num_bg_frames=100, start_frame=1):
         """estimate back ground from video
               num_bg_frames - number of (evenly spaced) frames (spannig whole video) over which to average (defaut 100)
         """
         frame_numbers = np.linspace(start_frame, self.vr.number_of_frames, num_bg_frames).astype(int)  # evenly sample movie
-        # self.frames = np.nan * np.zeros((num_bg_frames, self.vr.frame_width, self.vr.frame_height, self.vr.frame_channels), dtype=np.uint8)
-        for idx, fr in enumerate(frame_numbers):
+        for fr in frame_numbers:
             ret, frame = self.vr.read(fr)
             if ret and frame is not None:
                 self.update(frame)
-                # self.frames[idx, :, :] = frame
-        self.background = self.background / self.background_count#np.nanmedian(self.frames, axis=0)
+        self.background = self.background / self.background_count
+        return self.background
 
     def update(self, frame):
         """updates background (mean frame) with `frame`"""
-        # self.frames.append(frame)
         cv2.accumulate(frame, self.background)
         self.background_count += 1
 
     def save(self, file_name):
         """save `background` as file_name.PNG for later reference"""
         return cv2.imwrite(file_name, self.background)
+
+    def save_binary(self, file_name):
+        """Save `background` as file_name.NPY for later reference"""
+        return np.savez(file_name, slef.background)
 
     def load(self, file_name):
         """load `background` from file_name"""
@@ -57,27 +60,43 @@ class BackGround():
 
 
 class BackGroundMax(BackGround):
+    """Calculate background as maximum over frames."""
 
     def estimate(self, num_bg_frames=100, start_frame=1):
         """estimate back ground from video
               num_bg_frames - number of (evenly spaced) frames (spannig whole video) over which to average (defaut 100)
         """
         frame_numbers = np.linspace(start_frame, self.vr.number_of_frames, num_bg_frames).astype(int)  # evenly sample movie
-        # self.frames = np.nan * np.zeros((num_bg_frames, self.vr.frame_width, self.vr.frame_height, self.vr.frame_channels), dtype=np.uint8)
-        for idx, fr in enumerate(frame_numbers):
+        for fr in frame_numbers:
             ret, frame = self.vr.read(fr)
             if ret and frame is not None:
                 self.update(frame)
 
     def update(self, frame):
         """updates background (mean frame) with `frame`"""
-        # self.frames.append(frame)
         self.background = np.maximum(frame, self.background)  # element-wise max operation
         self.background_count += 1
 
 
 class BackGroundMedian(BackGround):
-    pass
+    """Calculate background as median over frames. Memory intensive."""
+
+    def estimate(self, num_bg_frames=100, start_frame=1):
+        """estimate back ground from video
+              num_bg_frames - number of (evenly spaced) frames (spannig whole video) over which to average (defaut 100)
+        """
+        frame_numbers = np.linspace(start_frame, self.vr.number_of_frames, num_bg_frames).astype(int)  # evenly sample movie
+        self.frames = []
+        for fr in frame_numbers:
+            ret, frame = self.vr.read(fr)
+            if ret and frame is not None:
+                self.update(frame)
+        self.background = np.nanmedian(self.frames, axis=0)
+
+    def update(self, frame):
+        """Updates background (mean frame) with `frame`."""
+        self.frames.append(frame)
+        self.background_count += 1
 
 
 if __name__ == "__main__":
@@ -86,7 +105,7 @@ if __name__ == "__main__":
     parser.add_argument('-n', '--num_bg_frames', type=int, default=100, help='number of frames for estimating background (100)')
     parser.add_argument('-f', '--format', type=str, default='png', help='image format for background (png)')
     parser.add_argument('-s', '--savebin', action='store_true', help='save as binary matrix (npy-format)')
-    parser.add_argument('-t', '--type', action='store', choices=['mean','max'], default='mean')
+    parser.add_argument('-t', '--type', action='store', choices=['mean','max', 'median'], default='mean')
 
     args = parser.parse_args()
 
@@ -100,4 +119,4 @@ if __name__ == "__main__":
 
         if args.savebin:
             print('saving raw background to {base}.npy'.format(base=os.path.splitext(args.filename)[0]))
-            np.savez('{base}.npy'.format(base=os.path.splitext(args.filename)[0]), bg.background)
+            bg.save_binary('{base}.npy'.format(base=os.path.splitext(args.filename)[0]))
