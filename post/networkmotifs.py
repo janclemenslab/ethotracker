89f5e0b2535f69a0177e26dc254e85d96c964b88
Jan Clemens <clemensjan@googlemail.com>
2018-05-04 16:06:42 +0200

    clean and fix

--- post/networkmotifs.py
+++ post/networkmotifs.py
@@ -11,20 +11,19 @@ def motif_counter(gr, mo, k):
 
     Args:
         gr: `DiGraph` object
-        mo: `dict` of motifs to count
+        dicmo: `dict` of motifs to count
         k: An `int` specifying motif length
     Returns:
         mcount: `dict` with the number of each motifs, with the same keys as ``mo``
 
-    From: https://gist.github.com/tpoisot/8582648
     This function is actually rather simple. It will extract all 3-grams from
     the original graph, and look for isomorphisms in the motifs contained
     in a dictionary. The returned object is a ``dict`` with the number of
     times each motif was found.::
         >>> print mcounter(gr, mo)
         {'S1': 4, 'S3': 0, 'S2': 1, 'S5': 0, 'S4': 3}
-
     """
+    # From: https://gist.github.com/tpoisot/8582648
     #This function will take each possible subgraph of gr of size 3, then
     #compare them to the mo dict using .subgraph() and is_isomorphic
     #This line simply creates a dictionary with 0 for all values, and the
@@ -65,7 +64,7 @@ def generate_motifs(k: int) -> typing.Dict[str, nx.DiGraph]:
             [a.extend(list(c)) for c in cc]  # flatten list
             all_edges_contained = np.unique(a).shape[0]>=k
             # make sure each edge exists only once in one direction in the motif
-            aa = [sorted(c) for c in list(cc)]  # list of all sorted (undirected) edges
+            aa = [c.sort() for c in list(cc)]  # list of all sorted (undirected) edges
             all_edges_unique = len(aa)==np.unique(aa, axis=0).shape[0]  # True only if all edges are unique
             # make sure the motif is a fully connected graph
             fully_connected = len(aa)>=k-1
@@ -117,6 +116,10 @@ def generate_special_motifs(kmin: int, kmax: int = 10) -> typing.Dict[str, nx.Di
 def process_motifs(chainer, chainee, max_k=4):
     """Count all motifs of size `max_k` in the network defined by chainer->chainee edges.
 
+    Args:
+        chainer: `list` [frames x ...]
+        chainer: `list` [frames x ...]
+        max_k=4:
     Returns:
         motif_counts: `list` of counts for each motif in `motifs`
         motifs: `dict`
@@ -129,7 +132,7 @@ def process_motifs(chainer, chainee, max_k=4):
         motifs[k] = generate_motifs(k)
         print(f"There exist {len(motifs[k])} topologically unique, directed motifs of size {k}")
     motifs.append(generate_special_motifs(max_k, 10))
-    
+
     # motifs in all frames
     nmotifs = sum([len(b) for b in motifs[2:]])
     motif_counts = np.zeros((nmotifs, nframes), dtype=np.uint16)
