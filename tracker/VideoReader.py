705047cea1f8520a91108994a5748df766967071
Jan Clemens <clemensjan@googlemail.com>
2018-05-04 23:08:15 +0200

    is now a list or a generator

--- tracker/VideoReader.py
+++ tracker/VideoReader.py
@@ -7,62 +7,93 @@ from contextlib import contextmanager
 class VideoReader:
     """wrapper around opencv's VideoCapture()
         USAGE
-            vr = VideoReader("video.avi", crop=[10, 20, 100, 80])
-            vr.read
+            vr = VideoReader("video.avi")  # initialize
+
+            # use as Sequence
+            vr[frame_number]
+            vr[0:1000:10000]
+            print(f'Video has {len(vr)} frames.')
+
+            # use as generator
+            for frame in vr.frames():
+                print(frame.shape)
         ARGS
-         file_name
-         crop       - crop coordinates [x1,y1,x2,y2]
-      VARS
+         filename
+        VARS
          frame_width, frame_height, frame_channels, frame_rate, number_of_frames, fourcc
       METHODS
-         read, reset, seek
-         close
+         ret, frame = vr.read(framenumber): read frame, for compatibility with opencv VideoCapture
+         vr.close(): release file
     """
-
-    def __init__(self, file_name, crop=None):
-        if not os.path.exists(file_name):
+    def __init__(self, filename):
+        if not os.path.exists(filename):
             raise FileNotFoundError
-        self._file_name = file_name
+        self._filename = filename
         self._vr = cv2.VideoCapture()
-        self._vr.open(self._file_name)
-        self._crop = crop
+        self._vr.open(self._filename)
         # read frame to test videoreader and get number of channels
         ret, frame = self.read()
         (self.frame_width, self.frame_height, self.frame_channels) = np.uintp(frame.shape)
         self.frame_shape = np.uintp(frame.shape)
-        self.number_of_frames = int(self._vr.get(cv2.CAP_PROP_FRAME_COUNT))
+        self.number_of_frames = len(self)
         self.frame_rate = int(self._vr.get(cv2.CAP_PROP_FPS))
         self.fourcc = int(self._vr.get(cv2.CAP_PROP_FOURCC))
 
     def __del__(self):
         self.close()
 
-    # standard usage
+    def __len__(self):
+        """Length is number of frames."""
+        return int(self._vr.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    def __getitem__(self, index):
+        """Now we can get frame via self[index] and self[start:stop:step]"""
+        if isinstance(index, slice):
+            return [self[ii] for ii in range(*index.indices(len(self)))]
+        return self.read(index)[1]
+
+    def __str__(self):
+        return f"{self._filename} with {len(self)} at {self.frame_rate} fps"
+
+    def frames(self, start=0, stop=None, step=1):
+        """generator that yields all frames"""
+        self._seek(start)
+        if stop==None:
+            stop = len(self)
+        # while True:
+        for frame_number in range(start, stop, step):
+            yield self._vr.read()[1]
+
+    # def __enter__(self):
+    #     return self
+    #
+    # def __exit__(self, *args):
+    #     self.close()
+
     def read(self, frame_number=None):
-        """read next frame or frame specified by `frame_number`"""
+        """Read next frame or frame specified by `frame_number`"""
         if frame_number is not None:  # seek
-            self.seek(frame_number)
+            self._seek(frame_number)
         ret, frame = self._vr.read()  # read
-        if ret and self._crop is not None:
-            frame = frame[self._crop[1]:self._crop[3], self._crop[0]:self._crop[2], :]
         return ret, frame
-
-    def reset(self):
-        """re-initialize object"""
-        self.__init__(self._file_name, crop=self._crop)
-
-    def seek(self, frame_number):
-        """go to frame"""
-        self._vr.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
 
     def close(self):
         self._vr.release()
 
+    def _reset(self):
+        """re-initialize object"""
+        self.__init__(self._filename)
+
+    def _seek(self, frame_number):
+        """go to frame"""
+        self._vr.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
+
+
 
 # as a context for working with `with` statements
 @contextmanager
-def video_file(path, crop=None):
-    vr = VideoReader(path, crop)
+def video_file(path):
+    vr = VideoReader(path)
     try:
         yield vr
     finally:
@@ -71,8 +102,8 @@ def video_file(path, crop=None):
 
 
 # as a frame generator
-def video_generator(path, crop=None):
-    with video_file("test/160125_1811_1.avi") as video:
+def video_generator(path):
+    with video_file(path) as video:
         while True:
             yield video.read()
 
@@ -84,6 +115,11 @@ def test():
     _, frame = vr1.read(100)
     cv2.imwrite("test/frame100.png", frame)
 
+    # as sequence
+    vrlist = VideoReader("test/160125_1811_1.avi")
+    frame100 = vrlist[100]
+    for frame in vrlist[::10000]:
+        print(frame.shape)
 
     # `with` statement
     print("testing as context")
@@ -102,4 +138,4 @@ def test():
         print(frame.shape)
 
 if __name__ == "__main__":
-    test()
+    pass# test()
