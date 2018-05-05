92e7da4ad2eb554cd0a0e112dc5415f01d885da3
Jan Clemens <clemensjan@googlemail.com>
2018-05-05 16:40:47 +0200

    more cleanup and context now in-class

--- tracker/VideoReader.py
+++ tracker/VideoReader.py
@@ -1,30 +1,36 @@
+"""Pythonic wrapper around opencv's VideoCapture()."""
 import numpy as np
 import os
 import cv2
-from contextlib import contextmanager
 
 
 class VideoReader:
-    """wrapper around opencv's VideoCapture()
-        USAGE
-            vr = VideoReader("video.avi")  # initialize
+    """Pythonic wrapper around opencv's VideoCapture().
 
-            # use as Sequence
-            vr[frame_number]
-            vr[0:1000:10000]
-            print(f'Video has {len(vr)} frames.')
-
-            # use as generator
-            for frame in vr.frames():
-                print(frame.shape)
-        ARGS
-         filename
-        VARS
-         frame_width, frame_height, frame_channels, frame_rate, number_of_frames, fourcc
-      METHODS
-         ret, frame = vr.read(framenumber): read frame, for compatibility with opencv VideoCapture
-         vr.close(): release file
+    USAGE
+        vr = VideoReader("video.avi")  # initialize
+        # use as Sequence
+        vr[frame_number]
+        vr[0:1000:10000]
+        print(f'Video has {len(vr)} frames.')
+        # use as generator/iterator
+        for frame in vr:
+            print(frame.shape)
+        # or to specify start frame
+        for frame in vr.frames(start_frame):
+            print(frame.shape)
+        # as context
+        with VideoReader("video.avi") as vr:
+            print(vr[0].shape)
+    ARGS
+        filename
+    PROPERTIES
+        frame_width, frame_height, frame_channels, frame_rate, number_of_frames, fourcc
+    METHODS
+        ret, frame = vr.read(framenumber): read frame, for compatibility with opencv VideoCapture
+        vr.close(): release file
     """
+
     def __init__(self, filename):
         if not os.path.exists(filename):
             raise FileNotFoundError
@@ -47,95 +53,81 @@ class VideoReader:
         return int(self._vr.get(cv2.CAP_PROP_FRAME_COUNT))
 
     def __getitem__(self, index):
-        """Now we can get frame via self[index] and self[start:stop:step]"""
+        """Now we can get frame via self[index] and self[start:stop:step]."""
         if isinstance(index, slice):
             return [self[ii] for ii in range(*index.indices(len(self)))]
         return self.read(index)[1]
 
     def __str__(self):
-        return f"{self._filename} with {len(self)} at {self.frame_rate} fps"
+        return f"{self._filename} with {len(self)} frames at {self.frame_rate} fps"
+
+    def __iter__(self):
+        return self.frames(start=0, stop=None, step=1)
 
     def frames(self, start=0, stop=None, step=1):
-        """generator that yields all frames"""
+        """Yield all frames (or a `slice` thereof).
+
+        ARGS
+            start=0
+            stop=number of frames
+            step=1
+        YIELDS
+            frame
+        """
         self._seek(start)
-        if stop==None:
+        if stop is None:
             stop = len(self)
-        # while True:
         for frame_number in range(start, stop, step):
             yield self._vr.read()[1]
 
-    # def __enter__(self):
-    #     return self
-    #
-    # def __exit__(self, *args):
-    #     self.close()
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        self.close()
 
     def read(self, frame_number=None):
-        """Read next frame or frame specified by `frame_number`"""
+        """Read next frame or frame specified by `frame_number`."""
         if frame_number is not None:  # seek
             self._seek(frame_number)
         ret, frame = self._vr.read()  # read
         return ret, frame
 
     def close(self):
+        """Release video file."""
         self._vr.release()
 
     def _reset(self):
-        """re-initialize object"""
+        """Re-initialize object."""
         self.__init__(self._filename)
 
     def _seek(self, frame_number):
-        """go to frame"""
+        """Go to frame."""
         self._vr.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
 
 
-
-# as a context for working with `with` statements
-@contextmanager
-def video_file(path):
-    vr = VideoReader(path)
-    try:
-        yield vr
-    finally:
-        vr.close()
-        vr = None
-
-
-# as a frame generator
-def video_generator(path):
-    with video_file(path) as video:
-        while True:
-            yield video.read()
-
-
-def test():
+def _test(filename="/Users/janc/rpi8-20180206_185652/rpi8-20180206_185652.mp4"):
     # standard usage
     print("testing as standard class")
-    vr1 = VideoReader("test/160125_1811_1.avi")
+    vr1 = VideoReader(filename)
+    print(vr1)
     _, frame = vr1.read(100)
-    cv2.imwrite("test/frame100.png", frame)
-
+    print(frame.shape)
     # as sequence
-    vrlist = VideoReader("test/160125_1811_1.avi")
-    frame100 = vrlist[100]
+    vrlist = VideoReader(filename)
+    frame = vrlist[100]
     for frame in vrlist[::10000]:
         print(frame.shape)
-
-    # `with` statement
+    # as context
     print("testing as context")
-    with video_file("test/160125_1811_1.avi") as vr2:
-        for _ in range(2):
-            ret, frame = vr2.read()
-            print(frame.shape)
-
-    print(vr2._vr.isOpened())
-
+    with VideoReader(filename) as vr2:
+        print(vr2[0].shape)
     # as generator
     print("testing as generator")
-    vid_gen = video_generator("test/160125_1811_1.avi")
-    for _ in range(2):
-        ret, frame = next(vid_gen)
+    vr3 = VideoReader(filename)
+    for frame in vr3.frames(start=100, stop=200, step=10):
         print(frame.shape)
 
+
 if __name__ == "__main__":
-    pass# test()
+    _test()
