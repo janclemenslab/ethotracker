a5c201dfb07cca607b4993a1f8b6ac73d7725ce2
Jan Clemens <clemensjan@googlemail.com>
2018-05-08 09:07:39 +0200

    change modulenames to lowercase

---
+++ tracker/background.py
@@ -0,0 +1,120 @@
+import numpy as np
+import cv2
+import argparse
+import os
+
+
+class BackGround():
+    """Calculate background as mean over frames.
+
+       ARGS
+          vr - VideoReader instance
+       VARS
+          background       - background estimate (avg frame)
+          background_count - number of frames which have been averaged to get `background`
+       METHODS
+          estimate(num_bg_frames=100) - estimate background from `num_bg_frames` covering whole video
+          save(file_name) - save `background` to file_name.PNG
+          load(file_name) - load `background` from file_name (uses cv2.imread)
+    """
+
+    def __init__(self, vr):
+        """Construct - vr is VideoReader instance."""
+        self.vr = vr
+        self.frames = []
+        self.background = np.zeros((self.vr.frame_width, self.vr.frame_height, self.vr.frame_channels))
+        self.background_count = 0
+
+    def estimate(self, num_bg_frames=100, start_frame=1):
+        """Estimate back ground from video.
+
+        `_accumulate` and then `_normalize` frames - these can be overridden.
+        Args:
+            num_bg_frames: number of (evenly spaced) frames (spannig whole video) over which to average (defaut 100)
+            start_frame: first frame to read
+        Returns:
+            background
+        """
+        frame_interval = int(len(self.vr)/num_bg_frames)
+        stop_frame = len(self.vr)
+        for frame in self.vr[start_frame:stop_frame:frame_interval]:
+            if frame is not None:
+                self._accumulate(frame)
+        self._normalize()
+        return self.background
+
+    def _accumulate(self, frame):
+        """Updates background with `frame`."""
+        cv2.accumulate(frame, self.background)
+        self.background_count += 1
+
+    def _normalize(self):
+        self.background = self.background / self.background_count
+
+    def save(self, file_name):
+        """Save `background` as file_name.PNG."""
+        return cv2.imwrite(file_name, self.background)
+
+    def save_binary(self, file_name):
+        """Save `background` as file_name.NPY."""
+        return np.savez(file_name, self.background)
+
+    def load(self, file_name):
+        """Load `background` from file_name."""
+        self.background = cv2.imread(file_name)
+
+
+class BackGroundMax(BackGround):
+    """Calculate background as maximum over frames."""
+
+    def _accumulate(self, frame):
+        """Update background (mean frame) with `frame`."""
+        self.background = np.maximum(frame, self.background)  # element-wise max operation
+        self.background_count += 1
+
+    def _normalize(self):
+        # nothing to do - overwrite parent function with this empty one
+        pass
+
+
+class BackGroundMedian(BackGround):
+    """Calculate background as median over frames. Memory intensive."""
+
+    def _accumulate(self, frame):
+        """_accumulates background (mean frame) with `frame`."""
+        self.frames.append(frame)
+        self.background_count += 1
+
+    def _normalize(self):
+        self.background = np.nanmedian(self.frames, axis=0)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename', type=str, help='video file to process')
+    parser.add_argument('-n', '--num_bg_frames', type=int, default=100, help='number of frames for estimating background (100)')
+    parser.add_argument('-f', '--format', type=str, default='png', help='image format for background (png)')
+    parser.add_argument('-s', '--savebin', action='store_true', help='save as binary matrix (npy-format)')
+    parser.add_argument('-t', '--type', action='store', choices=['mean','max', 'median'], default='mean')
+
+    args = parser.parse_args()
+    print(args)
+
+    from tracker.VideoReader import VideoReader
+    with VideoReader(args.filename) as vr:
+        if args.type == 'max':
+            bg = BackGroundMax(vr)
+        elif args.type == 'median':
+            bg = BackGroundMedian(vr)
+        else:
+            bg = BackGround(vr)
+
+        bg.estimate(num_bg_frames=args.num_bg_frames)
+
+        if args.format is not None:
+            print('saving background image as {base}.{format}'.format(base=os.path.splitext(args.filename)[0], format=args.format))
+            bg.save('{base}.{format}'.format(base=os.path.splitext(args.filename)[0], format=args.format))
+
+        if args.savebin:
+            print('saving raw background to {base}.npy'.format(base=os.path.splitext(args.filename)[0]))
+            bg.save_binary('{base}.npy'.format(base=os.path.splitext(args.filename)[0]))
---
+++ tracker/flypursuit.py
@@ -0,0 +1,170 @@
+"""Track videos."""
+import cv2
+import argparse
+import time
+import sys
+import traceback
+import os
+import logging
+import numpy as np
+
+from tracker.videoreader import VideoReader
+from tracker.attrdict import AttrDict
+import tracker.foreground as fg
+
+
+def annotate_frame(frame, res, raw_frame=True):
+    """Add centroids and lines to frame."""
+    uni_chambers = np.unique(res.chambers).astype(np.int)
+    frame_with_tracks = list()
+    for chamberID in range(np.max(uni_chambers)):
+        if raw_frame:
+            tmp = cv2.cvtColor(np.uint8(fg.crop(frame[:, :, 0], np.ravel(res.chambers_bounding_box[chamberID+1][:, ::-1]))), cv2.COLOR_GRAY2RGB)
+            tmp = tmp.astype(np.float32)/255.0
+        else:  # otherwise use background subtracted and thresholded frames
+            tmp = cv2.cvtColor(np.uint8(fg.crop(foreground, np.ravel(res.chambers_bounding_box[chamberID+1][:, ::-1]))), cv2.COLOR_GRAY2RGB)
+            tmp = tmp.astype(np.float32)
+        tmp = fg.annotate(tmp,
+                          centers=np.clip(np.uint(res.centers[res.frame_count, chamberID, :, :]), 0, 10000),
+                          lines=np.clip(np.uint(res.lines[res.frame_count, chamberID, 0:res.lines.shape[2], :, :]), 0, 10000))
+        frame_with_tracks.append(tmp)
+    return frame_with_tracks
+
+
+def display_frame(frame_with_tracks):
+    """Display list of frames."""
+    import matplotlib.pyplot as plt
+    plt.ion()
+    plt.clf()
+    plt.subplots_adjust(left=0.01, bottom=0.01, right=0.99, top=0.99, wspace=0.01, hspace=0.01)
+    for cnt, frame in enumerate(frame_with_tracks):
+        plt.subplot(1, len(frame_with_tracks), cnt+1)
+        plt.imshow(frame)
+        plt.axis('off')
+    # plt.tight_layout()
+    plt.show()
+    plt.pause(0.0001)
+
+
+def run(file_name, override=False, init_only=False, display=None, save_video=False, nflies=1,
+        threshold=0.4, save_interval=1000, start_frame=None, led_coords=[10, 550, 100, -1],
+        processor='chaining'):
+    """Track movie."""
+    if processor == 'chaining':
+        from tracker.frame_processor_chaining import Prc, init
+    elif processor == 'playback':
+        from tracker.frame_processor_playback import Prc, init
+    else:
+        raise TypeError(f'Unknown frame processor type {processor}. Should be `chaining` or `playback`.')
+
+    logging.info(f'processing {file_name}')
+    vr = VideoReader(file_name)
+
+    if not override:
+        try:  # attempt resume from intermediate results
+            # res = Results(file_name=os.path.normpath(file_name[:-4].replace('\\', '/') + '.h5'))
+            res = AttrDict().load(file_name=os.path.normpath(file_name[:-4].replace('\\', '/') + '.h5'))
+            res_loaded = True
+            if start_frame is None:
+                start_frame = res.frame_count
+            else:
+                res.frame_count = start_frame
+            logging.info(f'resuming from {start_frame}')
+        except KeyboardInterrupt:
+            raise
+        except Exception as e:  # if fails start from scratch
+            res_loaded = False
+            pass
+
+    if override or not res_loaded:  # re-initialize tracker
+        if start_frame is None:
+            start_frame = 0
+        logging.info('start initializing')
+        res = init(vr, start_frame, threshold, nflies, file_name, )
+        logging.info('done initializing')
+
+
+    if len(led_coords) != 4:
+        led_coords = fg.detect_led(vr[res.start_frame])
+
+    if init_only:
+        return
+
+    res.threshold = threshold
+    if save_video:
+        frame_size = tuple(np.uint(16 * np.floor(np.array(vr[0].shape[0:2], dtype=np.double) / 16)))
+        logging.warn('since x264 frame size need to be multiple of 16, frames will be truncated from {0} to {1}'.format(vr[0].shape[0:2], frame_size))
+        vw = cv2.VideoWriter(file_name[0:-4] + "tracks.avi", fourcc=cv2.VideoWriter_fourcc(*'X264'),
+                             fps=vr.frame_rate, frameSize=frame_size)
+
+    frame_processor = Prc(res)
+
+    # iterate over frames
+    start = time.time()
+    for frame in vr[start_frame:]:
+        try:
+            res, foreground = frame_processor.process(frame, res)
+            res.led[res.frame_count] = np.mean(fg.crop(frame, led_coords))
+
+            # get annotated frame if necessary
+            if save_video or (display is not None and res.frame_count % display == 0):
+                frame_with_tracks = annotate_frame(frame, res, raw_frame=True)
+
+            # display annotated frame
+            if display is not None and res.frame_count % display == 0:
+                display_frame(frame_with_tracks)
+
+            # save annotated frame to video
+            if save_video:
+                vw.write(np.uint8(frame_with_tracks[:frame_size[0], :frame_size[1], :]))
+
+            if res.frame_count % save_interval == 0:
+                logging.info('frame {0} processed in {1:1.2f}.'.format(res.frame_count, time.time() - start))
+                start = time.time()
+
+            if res.frame_count % save_interval == 0:
+                res.status = "progress"
+                res.save(file_name[0:-4] + '.h5')
+                logging.info("    saving intermediate results")
+        except KeyboardInterrupt:
+            raise
+        except Exception as e:  # catch errors during frame processing
+            logging.error(f'---- error processing frame {res.frame_count} ----')
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            traceback.print_tb(exc_traceback, limit=1, file=sys.stdout)
+            logging.error(repr(traceback.extract_tb(exc_traceback)))
+            ee = e
+            logging.error(ee)
+            # clean up - will be called before
+            if save_video:
+                vw.release()
+            vr.close()
+            return -1
+
+    # save results and clean up
+    logging.info("finished processing frames - saving results")
+    res.status = "done"
+    res.save(file_name[0:-4] + '.h5')
+    logging.info("             done.")
+    return 1
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file_name', type=str, help='video file to process')
+    parser.add_argument('--nflies', type=int, default=1, help='number of flies in video')
+    parser.add_argument('-d', '--display', type=int, default=None, help='show every Nth frame')
+    parser.add_argument('-t', '--threshold', type=float, default=0.4, help='threshold for foreground detection, defaults to 0.3')
+    parser.add_argument('-s', '--start_frame', type=float, default=None, help='first frame to track, defaults to 0')
+    parser.add_argument('-o', '--override', action='store_true', help='override existing initialization or intermediate results')
+    parser.add_argument('-p', '--processor', type=str, choices=['chaining', 'playback'], default='chaining', help='class to process frames')
+    parser.add_argument('--init_only', action='store_true', help='only initialize, do not track')
+    parser.add_argument('--save_video', action='store_true', help='save annotated vid with tracks')
+    parser.add_argument('--led_coords', nargs='+', type=int, default=[10, 550, 100, -1], help='should be a sequence of 4 values OTHERWISE will autodetect')
+    args = parser.parse_args()
+
+    logging.basicConfig(level=logging.INFO)
+    logging.info('Tracking {0} flies in {1}.'.format(args.nflies, args.file_name))
+
+    run(args.file_name, init_only=args.init_only, override=args.override, display=args.display, save_video=args.save_video,
+        nflies=args.nflies, threshold=args.threshold, start_frame=args.start_frame, led_coords=args.led_coords, processor=args.processor)
