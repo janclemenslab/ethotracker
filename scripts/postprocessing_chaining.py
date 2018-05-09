754024688487a05ae30a6def9c7e2c47e0995773
Jan Clemens <clemensjan@googlemail.com>
2018-05-09 19:19:35 +0200

    now?

--- scripts/postprocessing_chaining.py
+++ scripts/postprocessing_chaining.py
@@ -19,7 +19,11 @@ def load_data(file_name):
             lines = f['lines'][:]
             start_frame = f['start_frame'].value
     except KeyError as e:
-        pos, led, lines, start_frame = dd.io.load(file_name, ['/centers', '/led', '/lines', '/start_frame'])
+        f = dd.io.load(file_name)
+        pos = f['centers'][:]
+        led = f['led'][:]
+        lines = f['lines'][:]
+        start_frame = f['start_frame']        
     pos = pos[start_frame + 1:-1000, :, :]
     led = led[start_frame + 1:-1000, 0].T
 
