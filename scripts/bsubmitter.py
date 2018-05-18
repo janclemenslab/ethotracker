48b8ba1bcf4a39875116126f97017ed3eb04981f
Jan Clemens <clemensjan@googlemail.com>
2018-05-18 17:20:29 +0200

    fix videos

--- scripts/bsubmitter.py
+++ scripts/bsubmitter.py
@@ -2,9 +2,8 @@
 import os
 import glob
 
-
 # list all mp4 files
-root = '/scratch/clemens10/chaining'#'/Volumes/ukme04/#Common/chaining'#'Z:\#Common\chaining'#'/Volumes/ukme04/#Common/playback'#'/scratch/clemens10/playback'#
+root = '/Volumes/ukme04/#Common/chaining'#'Z:\#Common\chaining'#'/Volumes/ukme04/#Common/playback'#'/scratch/clemens10/playback'#
 datadir = f'{root}/dat'
 logdir = f'{root}/log'
 resdir = f'{root}/res'
@@ -15,24 +14,32 @@ videofiles.sort()
 print(videofiles)
 
 # build and execute command
-for videofile in videofiles:
+for filename in videofiles[-20:]:
     try:
-        basename = os.path.splitext(os.path.basename(videofile))[0]
-        basepath = os.path.splitext(videofile)[0]
-        print(os.path.splitext(videofile)[0] + '.nflies',)
-        # with open(os.path.splitext(videofile)[0] + '.nflies', 'r') as f:
+        basename = os.path.splitext(os.path.basename(filename))[0]
+        basepath = os.path.splitext(filename)[0]
+        # with open(os.path.splitext(filename)[0] + '.nflies', 'r') as f:
         #     nflies = f.read()
 
-        # cmd_track = f"python -m tracker.FlyPursuitChaining {videofile} --nflies {nflies} -o -t 0.25 --led_coords 0"
+        # cmd_track = f"python -m tracker.FlyPursuitChaining {filename} --nflies {nflies} -o -t 0.25 --led_coords 0"
         # cmd_post = f"python3 ~/Dropbox/code.py/analysis/scripts/postprocessing.py {basepath}.h5 {basepath}_snd.log {os.path.join(resdir,basename)}_spd.h5"
         # cmd_post = f"python ~/Dropbox/code.py/analysis/scripts/postprocessing.py {basepath}.h5 {basepath}_snd.log {os.path.join(resdir,basename)}_spd.h5"
-        cmd_post = f"python -m scripts.postprocessing_chaining {basepath}.h5 {basepath}_snd.log {os.path.join(resdir,basename)}_spd.h5"
-        # print(cmd_track)
-        # os.system(cmd_track)
+        # cmd_post = f"python -m scripts.postprocessing_chaining {basepath}.h5 {basepath}_snd.log {os.path.join(resdir,basename)}_spd.h5"
+        # # print(cmd_track)
+        # # os.system(cmd_track)
+        #
+        # print(cmd_post)
+        # os.system(cmd_post)
+        # print(f'cp {basepath}.h5 {resdir}')
+        # os.system(f'cp {basepath}.h5 {resdir}')
+        commands = [f"ffmpeg -i {filename} -vcodec copy {filename}.bak",  # Vcopy to new file
+               f"mv {filename}.bak {filename}",  # overwrite old file with new file
+               ]
 
-        print(cmd_post)
-        os.system(cmd_post)
-        os.system(f'cp {basepath}.h5 {resdir}')
+        for cmd in commands:
+            print(cmd)
+            # os.system(cmd)
+
     except (KeyboardInterrupt, SystemExit):
         raise
     except Exception as e:
