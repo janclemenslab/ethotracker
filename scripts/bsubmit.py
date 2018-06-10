d32326a2d4a3d6bc41acda70b084ac0c21068010
Jan Clemens <clemensjan@googlemail.com>
2018-06-10 09:52:47 +0200

    add queue arg, make work with angle fixer

--- scripts/bsubmit.py
+++ scripts/bsubmit.py
@@ -19,44 +19,49 @@ sm_tmpdir, sm_jobid = mo.groups()
 props = read_job_properties(jobscript)
 
 # set up job name, project name
-jobname = "{rule}-{jobid}".format(rule=props["rule"], jobid=sm_jobid)
+jobname = f"{props["rule"]}-{sm_jobid}"
 if props["params"].get("logid"):
-    jobname = "{rule}-{id}".format(rule=props["rule"], id=props["params"]["logid"])
+    jobname = f"{props["rule"]}-{props["params"]["logid"]}"
 
 if props["params"].get("runtime"):
     runtime = props["params"].get("runtime")
 else:
     runtime = "08:00"
 
+if props["params"].get("queue"):
+    queue = props["params"].get("queue")
+else:
+    queue = "mpi"
+
 # -E is a pre-exec command, that reschedules the job if the command fails
 #   in this case, if the data dir is unavailable (as may be the case for a hot-mounted file path)
-cmdline = 'bsub -J {jobname} -r -E "ls {datadir}" -W {runtime} '.format(jobname=jobname, datadir=DATADIR, runtime=runtime)
-# cmdline = 'bsub -J {jobname} -W {runtime}'.format(jobname=jobname, runtime="08:00")
+cmdline = f'bsub -J {jobname} -r -E "ls {DATADIR}" -W {runtime} -q {queue} '
 
 # log file output - "-oo" will overwrite existing log file, "-o" would append to existing file
 if props["params"].get("logfile"):
     namedir, namefile = os.path.split(props['params'].get('logfile'))
     logfilename = f"{namedir}/{jobname}-{namefile}"
 else:
-    logfilename = "{logdir}/{jobname}.txt".format(logdir=LOGDIR, jobname=jobname)
-cmdline += f"-oo {logfilename} "
+    logfilename = f"{LOGDIR}/{jobname}.txt"
+cmdline += f" -oo {logfilename} "
 
-# # pass memory resource request to LSF
-# mem = props.get('resources', {}).get('mem')
-# if mem:
-#     cmdline += '-R "rusage[mem={}]" -M {} '.format(mem, 2 * int(mem))
-cmdline += '-R scratch ' # request nodes with access to scratch
+# pass memory resource request to LSF
+if props["params"].get("memory"):
+    memory = props["params"].get("memory")
+    cmdline += f' -R "rusage[mem={memory}]" -M {memory} '
+
+cmdline += ' -R scratch ' # request nodes with access to scratch
 
 # figure out job dependencies
 dependencies = set(sys.argv[1:-3])
 if dependencies:
-    cmdline += "-w '{}' ".format(" && ".join(dependencies))
+    cmdline += " -w '{}' ".format(" && ".join(dependencies))
 
 # the actual job
 cmdline += jobscript
 
 # the success file
-cmdline += " %s/%s.jobfinished" % (sm_tmpdir, sm_jobid)
+cmdline += f" {sm_tmpdir}/{sm_jobid}.jobfinished "
 
 # the part that strips bsub's output to just the job id
 cmdline += r" | tail -1 | cut -f 2 -d \< | cut -f 1 -d \>"
