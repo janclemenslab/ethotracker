13fd61a0a0d65091302fdb4732f3202906b87926
Jan Clemens <clemensjan@googlemail.com>
2019-02-19 11:37:52 +0100

    works for all users via $HOME

--- scripts/bsubmit_block.sh
+++ scripts/bsubmit_block.sh
@@ -6,7 +6,7 @@ export OMP_NUM_THREADS=1
 export OPENBLAS_MAIN_FREE=1
 export MKL_DYNAMIC="FALSE"
 export OMP_DYNAMIC="FALSE"
-SCRIPTDIR="/usr/users/clemens10/analysis/scripts"
+SCRIPTDIR="$HOME/analysis/scripts"
 DATADIR="/scratch/clemens10/"
 LOGDIR="/scratch/clemens10/log"
 snakemake --rerun-incomplete --keep-going --nolock --notemp \
