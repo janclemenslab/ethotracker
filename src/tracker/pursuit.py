68df7308e354a7fd21a49ef24efb4fe4e9258c61
Jan Clemens <clemensjan@googlemail.com>
2019-03-11 14:01:51 +0100

    restrict nthreads

--- src/tracker/pursuit.py
+++ src/tracker/pursuit.py
@@ -1,8 +1,14 @@
+import os
+os.environ["OMP_NUM_THREADS"] = "1" # export OMP_NUM_THREADS=4
+os.environ["OPENBLAS_NUM_THREADS"] = "1" # export OPENBLAS_NUM_THREADS=4
+os.environ["MKL_NUM_THREADS"] = "1" # export MKL_NUM_THREADS=6
+os.environ["VECLIB_MAXIMUM_THREADS"] = "1" # export VECLIB_MAXIMUM_THREADS=4
+os.environ["NUMEXPR_NUM_THREADS"] = "1" # export NUMEXPR_NUM_THREADS=6
+
 """Track videos."""
 import platform
 import sys
 import traceback
-import os
 import logging
 import time
 from enum import Enum
