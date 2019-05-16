e67c6b36068f54aea41531a4c29e6cc283e0a69b
Deniz <denizyuzak@gmail.com>
2019-05-16 16:39:04 +0200

    Fixed fly size threshold.

--- src/ethotracker/tracker/frame_processor_playback.py
+++ src/ethotracker/tracker/frame_processor_playback.py
@@ -37,7 +37,7 @@ def init(vr, start_frame, threshold, nflies, file_name, num_bg_frames=100):
     # 2. segment and get flies and remove chamber if empty or "fly" too small
     labels = np.unique(res.chambers)
     area = np.array([fg.segment_center_of_mass(foreground * (res.chambers == label))[4] for label in labels])  # get fly size for each chamber
-    labels[area < 00] = 0                                                  # mark empty chambers for deletion
+    labels[area < 100] = 0                                                  # mark empty chambers for deletion
     res.chambers, _, _ = fg.clean_labels(res.chambers, labels, force_cont=True)  # delete empty chambers
     # 3. get bounding boxes for non-empty chambers for cropping
     res.chambers_bounding_box = fg.get_bounding_box(res.chambers)  # get bounding boxes of remaining chambers
