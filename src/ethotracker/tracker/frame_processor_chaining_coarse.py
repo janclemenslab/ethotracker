f57a426ef877253601845e2a1d4f1a53726828d8
Jan Clemens <clemensjan@googlemail.com>
2019-07-03 16:09:13 +0200

    fix non-commented line

--- src/ethotracker/tracker/frame_processor_chaining_coarse.py
+++ src/ethotracker/tracker/frame_processor_chaining_coarse.py
@@ -224,7 +224,8 @@ class Prc():
 
                     else:  # if still flies w/o conn compp fall back to segment_cluster
                         logging.info(f"{res.frame_count}: {flycnt[0]} outside of the conn comps or conn comp {np.where(flycnt[1:] == 0)} is empty - falling back to segment cluster - should mark frame as potential jump")
-                        centers[chb, :, :], labels, points,  = fg.segment_cluster(foreground_cropped, num_clusters=res.nflies)
+                        # centers[chb, :, :], labels, points,  = fg.segment_cluster(foreground_cropped, num_clusters=res.nflies)
+                        centers[chb, :, :], labels, points,  = fg.segment_cluster_sklearn(foreground_cropped, num_clusters=res.nflies, init_method=old_centers[chb, :, :])
                         frame_error[chb] = 3
 
                     if points.shape[0] > 0:   # make sure we have not lost the flies in the current frame
